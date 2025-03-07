6761cf659f4d3443aa3080e8939cab691523f4a5
Ali Ghrabi <alikghrabi@Alis-MacBook-Pro.local>
2025-03-07 03:06:35 +0200

    Adding input dictionary function

---
+++ merge-dictionaries.py
@@ -0,0 +1,13 @@
+def input_dictionary():
+    dic={}
+    while(True):
+        key=input("enter key or type exit to stop : ")
+        if key.lower() == 'exit':
+            break
+        value=input("enter value of the key:")
+        dic[key]=value
+    return dic
+
+
+usrdic = input_dictionary()
+print("Your Dictionary: ", usrdic)
